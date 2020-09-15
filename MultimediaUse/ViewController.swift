fab143d6e95c7a0e0813a8ce8b48c1bebec42df3
Hyunsoo Kim <gustnqpf3@gmail.com>
2020-09-15 16:05:44 +0900

    Initial Commit

---
+++ MultimediaUse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultimediaUse
+//
+//  Created by 김현수 on 2020/09/15.
+//  Copyright © 2020 Hyun Soo Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
